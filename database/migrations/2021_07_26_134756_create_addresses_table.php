096fc2e4d7f1eb3b68361d1fd5ff8d7e53f5ecdf
Florent G <florentgleizes@hotmail.com>
2021-07-26 16:33:31 +0200

    ajout flipbox/lumen-generator, et creation fichier migration addresses

---
+++ database/migrations/2021_07_26_134756_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('number')->nullable();
+            $table->string('street', 60);
+            $table->string('additionnal_address', 60)->nullable();
+            $table->string('building', 20)->nullable();
+            $table->tinyInteger('floor')->nullable();
+            $table->string('residence', 20)->nullable();
+            $table->string('staircase', 2)->nullable();
+            $table->foreignId('city_id')->constrained('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
