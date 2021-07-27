6e39d3eb1a2d88f751083c63be8f8a97d79e0894
Florent G <florentgleizes@hotmail.com>
2021-07-27 16:26:47 +0200

    creation des routes login/register et authController

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,62 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+/*
+|--------------------------------------------------------------------------
+| Routes Person
+|--------------------------------------------------------------------------
+*/
+
+$router->get('user', 'AuthController@login');
+$router->post('user', 'AuthController@register');
+
+// $router->get('user', function () {
+//     return 'Hello World';
+// });
+
+// $router->get('user/{id}', ['as' => 'profile', function ($id) {
+//     return 'Hello User id:' . $id;
+// }]);
+
+// $router->post('user', ['as' => 'signin', function () {
+//     return 'User added';
+// }]);
+
+// $url = route('signin', ['id' => 1]);
+
+// $router->put('user/{id}', function ($id) {
+//     return 'User id:' . $id . ' updated';
+// });
+
+// $router->delete('user/{id}', function ($id) {
+//     return 'User id:' . $id . ' deleted';
+// });
+
+$router->group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () use ($router) {
+
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('me', 'AuthController@me');
+});
+
+// $router->group(['middleware' => 'auth'], function () use ($router) {
+//     $router->get('/', function () {
+//         // Uses Auth Middleware
+//     });
+
+//     $router->get('user/profile', function () {
+//         // Uses Auth Middleware
+//     });
+// });
+
+
+
 // Pour générer une clée aléatoire à copier dans .env APP_KEY
 // if (!app()->environment('prod'))     $router->get('/key', function () {
 //     return 'APP_KEY=base64:' . base64_encode(\Illuminate\Support\Str::random(32));
