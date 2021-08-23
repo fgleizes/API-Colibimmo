c50a4a3e0dce089e3b9e0eb22f3a988b501ada0c
julien irlinger <julienirlinger@gmail.com>
2021-08-23 10:52:56 +0200

    add model note/type-appointment

---
+++ app/Models/Type_appointment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type_appointment extends Model
+{
+    protected $table = 'type_appointment';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name'];
+}
