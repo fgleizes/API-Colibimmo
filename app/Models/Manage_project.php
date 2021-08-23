754565c5055a6beb6945f2a32635b27246d4881a
julien irlinger <julienirlinger@gmail.com>
2021-08-23 10:18:37 +0200

    add model favorite/manageProject/personProject

---
+++ app/Models/Manage_project.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Manage_project extends Model
+{
+    protected $table = "manage_project";
+    protected $primaryKey = 'id';
+}
---
+++ app/Models/Person_appointment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person_appointment extends Model
+{
+    protected $table = "person_appointment";
+    protected $primaryKey = 'id';
+}
