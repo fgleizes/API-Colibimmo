2f567d73b79b6dbc224c05012ba4eb75e4973f38
fjquen <fjquen@gmail.com>
2022-02-10 12:22:18 +0100

    debug ShowProjectByTYpe OK

--- app/Models/Type_project.php
+++ app/Models/Type_project.php
@@ -8,11 +8,7 @@ class Type_project extends Model
 {
     protected $table = 'type_project';
     protected $primaryKey = "id";
-    protected $fillable = ['name'];    
-
-    // public function filter_type_project(){
-    //     return $this->hasMany(Project::class,'id_Type_project');
-    // }
+    protected $fillable = ['name'];
 }
 
 
